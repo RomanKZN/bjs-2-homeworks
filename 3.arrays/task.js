d8d758bcf0d61f305e83903383569657a6ec36ea
Roman Medvedev <romankazan88@gmail.com>
2022-10-15 18:15:02 +0500

    task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length === arr2.length)
+    return arr1.every((a, index) => a === arr2[index])
+  else 
+    return false; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(arr => arr >= 0).filter(arr => arr % 3 == 0).map(arr => arr * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+   return resultArr; // array
 }
