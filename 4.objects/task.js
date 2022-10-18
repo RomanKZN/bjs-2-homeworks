8cc01ea72cd83e2eacbb4dcd3630ddf20929e5c1
Roman Medvedev <romankazan88@gmail.com>
2022-10-18 11:07:49 +0500

    task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,33 @@
-function Student(name, gender, age) {
-    // Ваш код
-
-}
+function Student(name, gender, age) {}
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+};
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function (mark1, mark2, mark3, ...other) {
+  this.marks = [mark1, mark2, mark3, ...other];
+};
+
+Student.prototype.getAverage = function () {
+  if (this.marks.length === 0) {
+    return 0;
+  }
+  let summ = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    summ += this.marks[i];
+  }
+  return summ / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete Student.setSubject;
+  delete Student.addMarks;
+  this.excluded = reason;
+};
