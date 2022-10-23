6232062edf2d95958cfadd2f40fb7ac9ef440bea
Roman Medvedev <romankazan88@gmail.com>
2022-10-23 15:29:09 +0500

    first commit

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,52 @@
+// Задача 1
+
+function parseCount(string) {
+  let parsed = Number.parseInt(string);
+  if (isNaN(parsed)) {
+    throw new Error("Невалидное значение");
+  }
+  return parsed;
+}
+
+function validateCount(base) {
+  try {
+    return parseCount(base);
+  } catch (error) {
+    return error;
+  }
+}
+
+// Задача 2
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a > b + c || b > a + c || c > a + b) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+  getPerimeter() {
+    let perimetr = this.a + this.b + this.c;
+
+    return perimetr;
+  }
+  getArea() {
+    let s = (this.a + this.b + this.c) / 2;
+    let area = Math.sqrt(s * ((s - this.a) * (s - this.b) * (s - this.c)));
+
+    return +area.toFixed(3);
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      getPerimeter: () => "Ошибка! Треугольник не существует",
+      getArea: () => "Ошибка! Треугольник не существует",
+    };
+  }
+}
