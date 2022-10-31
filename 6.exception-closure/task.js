fffbf206beb8660451adc5a20785a1902c534a78
Roman Medvedev <romankazan88@gmail.com>
2022-10-31 10:43:02 +0500

    correction of the first task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -23,17 +23,17 @@ class Triangle {
     this.a = a;
     this.b = b;
     this.c = c;
+    
     if (a > b + c || b > a + c || c > a + b) {
       throw new Error("Треугольник с такими сторонами не существует");
     }
   }
   getPerimeter() {
-    let perimetr = this.a + this.b + this.c;
-
+    let perimetr = this.a + this.b + this.c ;
     return perimetr;
   }
   getArea() {
-    let s = (this.a + this.b + this.c) / 2;
+    let s = this.getPerimeter () / 2;
     let area = Math.sqrt(s * ((s - this.a) * (s - this.b) * (s - this.c)));
 
     return +area.toFixed(3);
